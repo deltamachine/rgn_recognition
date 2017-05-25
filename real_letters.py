50ff3a36f9d8b82bec7f4be1e82a261b0a1d9b4c
Anna Kondratjeva <an-an-kondratjeva@yandex.ru>
2017-05-25 11:33:40 +0300

    Update real_letters.py

--- real_letters.py
+++ real_letters.py
@@ -1,34 +1,29 @@
-import os
 import re
+import sys
 
-def edit_box():
-    list_of_files = os.listdir(os.getcwd() + '/training_data')
-    counter = len(list_of_files) // 3
-    
-    os.chdir(os.getcwd() + '/training_data')
 
-    for i in range (counter):
-        box_name = 'rgn.handwriting.exp' + str(i) + '.box'
-        txt_name = 'rgn.handwriting.exp' + str(i) + '.txt'
+def edit_box(box_name, txt_name, output):
+    with open (txt_name, 'r', encoding = "utf-8") as file:
+        real_text = file.read()
 
-        with open (txt_name, 'r', encoding = "utf-8") as file:
-            real_text = file.read()
+    real_text = re.sub(' ', '', real_text)
+    real_text = re.sub('\n', '', real_text)
 
-        real_text = re.sub(' ', '', real_text)
-        real_text = re.sub('\n', '', real_text)
+    with open (box_name, 'r', encoding = "utf-8") as file:
+        box_junk = file.read().split('\n')
 
-        with open (box_name, 'r', encoding = "utf-8") as file:
-            box_junk = file.read().split('\n')
+    with open (output, 'w', encoding = "utf-8") as file:
+        for i in range (len(real_text)):
+            brand_new_box = box_junk[i].split(' ')
+            brand_new_box[0] = real_text[i]
+            file.write ('%s%s' % (' '.join(brand_new_box), '\n'))
 
-        with open (box_name, 'w', encoding = "utf-8") as file:
-            for i in range (len(real_text)):
-                brand_new_box = list(box_junk[i])
-                brand_new_box[0] = real_text[i]
-                file.write (''.join(brand_new_box))
-                file.write ('\n')
-        
 def main():
-    edit_box()
+    box_name = sys.argv[1]
+    txt_name = sys.argv[2]
+    output = sys.argv[3]
+        
+    edit_box(box_name, txt_name, output)
 
 if __name__ == '__main__':
     main()
